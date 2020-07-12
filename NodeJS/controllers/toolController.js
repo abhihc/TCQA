c2ff6857b9cddd962893057dbc17b366efe843df
Ivan Jovanovikj <ivanj@mail.uni-paderborn.de>
2020-07-12 12:50:48 +0200

    Fixes the problem with the PUT method

--- NodeJS/controllers/toolController.js
+++ NodeJS/controllers/toolController.js
@@ -7,8 +7,7 @@ var { Tool } = require('../models/tool');
 // => localhost:3000/tools/
 router.get('/', (req, res) => {
     Tool.find((err, docs) => {
-        if (!err) { res.send(docs); }
-        else { console.log('Error in Retriving Tool :' + JSON.stringify(err, undefined, 2)); }
+        if (!err) { res.send(docs); } else { console.log('Error in Retriving Tool :' + JSON.stringify(err, undefined, 2)); }
     });
 });
 
@@ -20,8 +19,7 @@ router.post('/', (req, res) => {
         toolInfo: req.body.toolInfo
     });
     tool.save((err, doc) => {
-        if (!err) { res.send(doc); }
-        else { console.log('Error in quality Save :' + JSON.stringify(err, undefined, 2)); }
+        if (!err) { res.send(doc); } else { console.log('Error in quality Save :' + JSON.stringify(err, undefined, 2)); }
     });
 });
 
@@ -34,9 +32,15 @@ router.put('/:id', (req, res) => {
         toolName: req.body.toolName,
         toolInfo: req.body.toolInfo
     });
-    Tool.findByIdAndUpdate(req.params.id, { $set: tool }, { new: true }, (err, doc) => {
-        if (!err) { res.send(doc); }
-        else { console.log('Error in tool update :' + JSON.stringify(err, undefined, 2)); }
+
+    var toolData = tool.toObject();
+
+    delete toolData._id;
+
+    var query = { _id: req.params.id };
+
+    Tool.updateOne(query, toolData, { upsert: true }, (err, doc) => {
+        if (!err) { res.send(doc); } else { console.log('Error in tool update :' + JSON.stringify(err, undefined, 2)); }
     });
 });
 
@@ -45,8 +49,7 @@ router.delete('/:id', (req, res) => {
         return res.status(400).send(`No record with given id : ${req.params.id}`);
 
     Tool.findByIdAndRemove(req.params.id, (err, doc) => {
-        if (!err) { res.send(doc); }
-        else { console.log('Error in tool detail Delete :' + JSON.stringify(err, undefined, 2)); }
+        if (!err) { res.send(doc); } else { console.log('Error in tool detail Delete :' + JSON.stringify(err, undefined, 2)); }
     });
 });
 
